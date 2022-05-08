3b3df19b2fdd01722e6471425a9ca7c62e028d37
Firez <55920870+FirezTheGreat@users.noreply.github.com>
2022-05-08 14:02:22 +0530

    Update config.example.mjs

--- config.example.mjs
+++ config.example.mjs
@@ -1,12 +1,5 @@
 const Config = {
-<<<<<<< HEAD
     token: '' //TOKEN here
 };
 
 export default Config;
-=======
-    token: ''
-};
-
-export default Config;
->>>>>>> ff070f38660d4b18b8abda588b88ea58293053bb
