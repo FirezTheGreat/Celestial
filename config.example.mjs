5c464c18ba1fca055697f28690c1ca6feb5fe06b
FirezTheGreat <marshalrony123@gmail.com>
2022-05-09 11:16:00 +0530

    Using Lavalink as djs/voice docs are incomplete

--- config.example.mjs
+++ config.example.mjs
@@ -1,6 +1,5 @@
 const Config = {
     token: '', //Discord Bot Token - https://discord.com/developers/applications
-    youtube_api_key: '' //Youtube API Key - https://console.cloud.google.com/apis/credentials
 };
 
 export default Config;
